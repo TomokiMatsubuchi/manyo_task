91c5cb5953919e8b44d21d6ec27fc0ea2d081bb9
tomoki matsubuchi <matsubuchitmk@gmail.com>
2022-10-25 21:58:08 +0900

    herokudeploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-taro = User.create!(name:'taro', email: 'tatotarotaros@sample.com', password: 'password', password_confirmation: 'password')
-current_hanako = User.create!(name: 'hanako', email: 'hanahanahanakosan@sample.com', password: '1234567', password_confirmation: '1234567', admin: "yes")
+taro = User.create!(name:'taro', email: 'tttttatotarotaros@sample.com', password: 'password', password_confirmation: 'password')
+current_hanako = User.create!(name: 'hanako', email: 'hhhhhhhhhanahanahanakosan@sample.com', password: '1234567', password_confirmation: '1234567', admin: "true")
 
 50.times do |i|
   Task.create!(title: "hanako_task_title_#{ i + 1 }", content: "testデータ", deadline_on: Date.current + i, priority: "中", status: Task.statuses.values.to_a.sample, user_id: current_hanako.id)
